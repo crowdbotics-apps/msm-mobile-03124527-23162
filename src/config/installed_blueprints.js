7a41c95bb0b5a2ad8a4681ccf424fc7083da2bc6
manan-crowdbotics <61427302+manan-crowdbotics@users.noreply.github.com>
2020-12-03 17:03:45 +0530

    Update installed_blueprints.js

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -2,6 +2,7 @@ export const installed_blueprints = [
   //@BlueprintInsertion
 { name: 'Home', human_name: 'Home', access_route: 'BlankScreen0181703'},
 { name: 'Onboarding', human_name: 'Onboarding', access_route: 'OnboardingSlideScreen2181704'},
+{ name: 'privacyPolicy', human_name: 'Privacy Policy', access_route: 'TermsScreen3181705'}
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
 ];
