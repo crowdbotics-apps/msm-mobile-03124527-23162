85ededd835b3643a82ffc613301f8ba95990db19
manan-crowdbotics <61427302+manan-crowdbotics@users.noreply.github.com>
2020-12-03 16:40:07 +0530

    Update index.js

--- src/features/OnboardingSlideScreen2181704/screens/index.js
+++ src/features/OnboardingSlideScreen2181704/screens/index.js
@@ -61,7 +61,7 @@ export default class Blank extends React.Component {
   }
 
   _onDone = () => {
-    this.props.navigation.navigate('BlankScreen2177769') // Login/Sign up Screen
+    this.props.navigation.navigate('BlankScreen0181703') // Login/Sign up Screen
   }
 
 
