7f7e7e2b6cda027f1f0556f249397aaeef46a3b8
manan-crowdbotics <61427302+manan-crowdbotics@users.noreply.github.com>
2020-12-03 16:45:52 +0530

    Update mainNavigator.js

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -6,6 +6,7 @@ import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import BlankScreen0181703Navigator from '../features/BlankScreen0181703/navigator';
+import OnboardingSlideScreen2181704Navigator from '../features/OnboardingSlideScreen2181704/navigator';
 
 /**
  * new navigators can be imported here
@@ -15,6 +16,7 @@ const AppNavigator = {
 
     //@BlueprintNavigationInsertion
 BlankScreen0181703: { screen: BlankScreen0181703Navigator },
+OnboardingSlideScreen2181704: { screen: OnboardingSlideScreen2181704Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
